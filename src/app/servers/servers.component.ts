8697b6c56fad34f6ed01fa4a734edfe68c537923
tokingfisher19 <tokingfisher19@gmail.com>
2019-09-11 11:51:27 +0600

    Component Selector, Databinding, String interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+// @ts-ignore
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'app-servers',
+  // selector: '[app-servers]', // atribute selector
+  // tslint:disable-next-line:component-selector
+  // selector: '.app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
