ad05641d18f25d0de03c6244523075af7f9de820
tokingfisher19 <tokingfisher19@gmail.com>
2019-09-15 18:30:13 +0600

    http request and fetch data from server

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,19 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PersonsService} from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector : 'app-persons',
     templateUrl : './persons.component.html'
 })
 
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
     personList: string[];
+    private personListSubs = Subscription;
     // private  personService: PersonsService; // adding service
     constructor( private perService: PersonsService) {
         // this.personList = perService.personList;
         // this.personService = perService;
     }
     ngOnInit() {
-      this.personList = this.perService.personList;
+      // this.personList = this.perService.personList;
+      // @ts-ignore
+      this.personListSubs = this.perService.personUpdate.subscribe(personList => {
+            this.personList = personList;
+          });
+      this.perService.fetchPersons();
+    }
+    removePerson(name: string) {
+      this.perService.personRemove(name);
+      console.log(name);
+    }
+    ngOnDestroy() {
+      this.personListSubs.unsubscribe();
     }
 }
